acf59c2e2642d44a72dd5e1d1551d53f966d42b2
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-05-22 10:34:52 +0200

    feat: async pipe operator

---
+++ mod.ts
@@ -0,0 +1,31 @@
+type AnyFunc = (...arg: any[]) => Promise<any> | any;
+
+type LastFnReturnType<F extends Array<AnyFunc>, Else = never> = F extends [
+  ...any[],
+  (...arg: any) => infer R
+]
+  ? R
+  : Else;
+
+type PipeArgs<F extends AnyFunc[], Acc extends AnyFunc[] = []> = F extends [
+  (...args: infer A) => infer B
+]
+  ? [...Acc, (...args: A) => B | Promise<B>]
+  : F extends [(...args: infer A) => any, ...infer Tail]
+  ? Tail extends [(arg: infer B) => any, ...any[]]
+    ? PipeArgs<Tail, [...Acc, (...args: A) => B | Promise<B>]>
+    : Acc
+  : Acc;
+
+export const pipe =
+  <FirstFn extends AnyFunc, F extends AnyFunc[]>(
+    firstFn: FirstFn,
+    ...fns: PipeArgs<F> extends F ? F : PipeArgs<F>
+  ) =>
+  (
+    ...args: Parameters<FirstFn>
+  ): Promise<LastFnReturnType<F, ReturnType<FirstFn>>> =>
+    (fns as AnyFunc[]).reduce(
+      (acc, fn) => (acc instanceof Promise ? acc.then(fn) : fn(acc)),
+      firstFn(...args)
+    );
